a834f247d7c3a885424e6e81960857ac7711c5ad
remi <remi@148.fr>
2015-11-30 22:13:42 +0100

    modifs Axelle

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,8 @@
-$(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html,body').animate({
                     scrollTop: target.offset().top
@@ -11,4 +11,22 @@ $(function() {
             }
         }
     });
+
+    $('#fixed_btn').on('click', function () {
+        var audio = document.getElementById('audio');
+        var play = document.getElementById('play');
+        var pause = document.getElementById('pause');
+
+        var volume = audio.volume;
+        if (volume < 1) {
+            audio.volume = 1;
+            pause.style.display = "block";
+            play.style.display = "none";
+        } else {
+            audio.volume = 0;
+            pause.style.display = "none";
+            play.style.display = "block";
+        }
+    })
+
 });
